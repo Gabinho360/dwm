131fe2cf06fcc26df323bdd037a2b998b0a311b9
Gabinho360 <gabriel360.d@gmail.com>
2025-03-09 19:40:08 -0300

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -27,7 +27,7 @@ static const char *const autostart[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "󰬺", "󰬻", "󰬼", "󰬽", "󰬾" };
+static const char *tags[] = { "1", "2", "3", "4", "5" };
 
 static const Rule rules[] = {
 	/* xprop(1):
