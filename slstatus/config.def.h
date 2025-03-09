6e1437bd964e27f4a0050f3e2d2951c65b3c70dc
Gabinho360 <gabriel360.d@gmail.com>
2025-03-09 19:09:48 -0300

    Update config.def.h

--- slstatus/config.def.h
+++ slstatus/config.def.h
@@ -6,11 +6,11 @@ static const char unknown_str[] = "n/a";
 
 static const struct arg args[] = {
 	/* function format          argument */
+//	{ wifi_essid, "  %s", "wlp0s20f3" },
 	{ cpu_perc, "  %s%%", NULL },
 	{ ram_used, "  %s", NULL },
 	{ ram_total, "/%s", NULL },
 //	{ battery_perc, "  %s%%", "BAT0" },
-//      { wifi_essid, "  %s", "wlp0s20f3" },
-//	{ datetime, "  %s", "%d/%m/%Y 󰥔 %H:%M 󰣇 " },
+//	{ datetime, "  %s", "%d/%m 󰥔 %H:%M 󰣇 " },
 	{ datetime, "  %s", "%d/%m/%Y 󰥔 %H:%M:%S 󰣇 " },
 };
